0782b4eb22c51ac3bdb7bdbc328b18ce8c829e98
emrahsoytekin <emrahsoytekin@gmail.com>
2022-01-03 23:56:18 +0300

    added logrus data values

--- formatter.go
+++ formatter.go
@@ -5,13 +5,14 @@ import (
 	"github.com/sirupsen/logrus"
 	"os"
 	"path/filepath"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 const (
 	// Default log format will output [INFO]: 2006-01-02T15:04:05Z07:00 - Log message
-	defaultLogFormat       = "%lvl% [ %time% ] %file% %func% => %msg%\n"
+	defaultLogFormat       = "%lvl% [ %time% ] %file% %func% => %msg% %fields%\n"
 	defaultTimestampFormat = "2006-01-02 15.04.05"
 )
 
@@ -61,6 +62,23 @@ func (f *Formatter) Format(entry *logrus.Entry) ([]byte, error) {
 	level := fmt.Sprintf("%-5s", strings.ToUpper(entry.Level.String()))
 	output = strings.Replace(output, "%lvl%", colorFormat.Color(entry, level), 1)
 
+	var fields string
+
+	// Put keys in a string array and sort it.
+	keys := make([]string, len(entry.Data))
+	i := 0
+	for k := range entry.Data {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+
+	// Do the rest.
+	for _, key := range keys {
+		fields = fmt.Sprintf("%s %s=%v", fields, colorFormat.Color(entry, key), entry.Data[key])
+	}
+
+	output = strings.Replace(output, "%fields%", fields, 1)
 	for k, val := range entry.Data {
 		switch v := val.(type) {
 		case string:
